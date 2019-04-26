5bace612426a780fd86469e89dd9fe672b1dbb6b
Chase Lirette <clirette@dxc.com>
2019-04-26 11:49:11 -0500

    More refactoring

--- src/index.js
+++ src/index.js
@@ -26,23 +26,15 @@ const App = () => {
 
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img
-            src="https://en.gravatar.com/userimage/153747069/41e965a07cb3510c435949f510f29907.jpg"
-            alt="avatar"
-          />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Chase
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <Comment
+        avatar="https://en.gravatar.com/userimage/153747069/41e965a07cb3510c435949f510f29907.jpg"
+        firstName="Chase"
+        date={faker.date.weekday()}
+        time={faker.date
+          .recent()
+          .toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
+        commentText="React is cool!"
+      />
 
       {comments}
     </div>
