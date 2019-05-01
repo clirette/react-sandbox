72941e83ee4cd629dde79fc9b6acdfd5339fa197
Chase Lirette <chaselirette@yahoo.com>
2019-05-01 02:00:45 -0500

    Added ApprovalCard

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Comment from "./components/Comment";
+import ApprovalCard from "./components/ApprovalCard";
 import faker from "faker";
 
 const MAX_COMMENTS = 10;
@@ -11,15 +12,17 @@ const App = () => {
   function generateComment(iterations) {
     for (let i = 0; i < iterations; i++) {
       comments.push(
-        <Comment
-          avatar={faker.image.avatar()}
-          firstName={faker.name.firstName()}
-          date={faker.date.weekday()}
-          time={faker.date
-            .recent()
-            .toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
-          commentText={faker.hacker.phrase()}
-        />
+        <ApprovalCard>
+          <Comment
+            avatar={faker.image.avatar()}
+            firstName={faker.name.firstName()}
+            date={faker.date.weekday()}
+            time={faker.date
+              .recent()
+              .toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
+              commentText={faker.hacker.phrase()}
+          />
+        </ApprovalCard>
       );
     }
   }
@@ -30,14 +33,15 @@ const App = () => {
     <div className="container">
       <div className="ui comments">
         <h3 className="ui dividing header">Comments</h3>
-        <Comment
-          avatar="https://en.gravatar.com/userimage/153747069/41e965a07cb3510c435949f510f29907.jpg"
-          firstName="Chase"
-          date="Monday"
-          time="3:00 PM"
-          commentText="React is cool!"
-        />
-
+        <ApprovalCard>
+          <Comment
+            avatar="https://en.gravatar.com/userimage/153747069/41e965a07cb3510c435949f510f29907.jpg"
+            firstName="Chase"
+            date="Monday"
+            time="3:00 PM"
+            commentText="React is cool!"
+          />
+        </ApprovalCard>
         {comments}
       </div>
     </div>
