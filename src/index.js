298562f3c60b68861dc78410efb4326840af25fa
Chase Lirette <clirette@dxc.com>
2019-05-01 13:54:55 -0500

    Check weather through location API, use loading spinner component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: '' };
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Spinner message="Please Accept Location Request" />;
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
+  }
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
